d509b90c5d1bf108a32235c55996c61b9b6a4a5a
Sameer <11097096+sameer@users.noreply.github.com>
2018-11-13 20:08:07 -0600

    Shorten 2

--- solutions/2.py
+++ solutions/2.py
@@ -1,2 +1,2 @@
 s = str
-[print(x) for x in range(1, 999999) if (x % 3 == 0 or x % 5 == 0 or x % 7 == 0) and (x < 10 or s(x)[0] == s(x)[-1])]
+[print(x) for x in range(1, 999999) if (x % 3 == 0 or x % 5 == 0 or x % 7 == 0) and s(x)[0] == s(x)[-1]]
